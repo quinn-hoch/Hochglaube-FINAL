befedfa9cf5cc2d7c80c3bd8f32306350b1c7af9
quinn-hoch <qh2202@nyu.edu>
2024-12-04 14:20:54 -0500

    not initial

--- sketch.js
+++ sketch.js
@@ -30,56 +30,88 @@
 */
 
 let vid;
+let audio;
 let vid2;
+let audio2;
 let vid3;
+let audio3;
 let vid4;
+let audio4;
 let c = 0;
+let angrybaby;
+let happybaby;
 
 function setup() {
   createCanvas(640, 480);
-  
-  
-  vid = createVideo('libraries/tuckercarlson.mov');
-  vid2 = createVideo('libraries/tcarlson2.mov');
-  vid3 = createVideo('libraries/cavuto.mov');
-  vid4 = createVideo('libraries/hannity.mov');
 
+  vid = createVideo('libraries/tuckerbidenvideo.mov'); //('libraries/tuckercarlson.mov')
+  audio = createAudio('libraries/tuckerbidenaudio.mov');
+  audio1 = createAudio('libraries/tuckerbidenaudio.mov');
+  vid2 = createVideo('libraries/tuckermediavideo.mov'); //('libraries/tcarlson2.mov')
+  audio2 = createAudio('libraries/tuckermediaaudio.mov');
+  audio21 = createAudio('libraries/tuckermediaaudio.mov');
+  vid3 = createVideo('libraries/cavutovideo.mov');
+  audio3 = createAudio('libraries/cavutoaudio.mov');
+  audio31 = createAudio('libraries/cavutoaudio.mov');
+  vid4 = createVideo('libraries/hannityvideo.mov');
+  audio4 = createAudio('libraries/hannityaudio.mov');
+  audio41 = createAudio('libraries/hannityaudio.mov');
+  happybaby = createAudio('libraries/happybaby.mp3');
+  angrybaby = createAudio('libraries/cryingbaby.mp3');
+
+  // vid.loop();
+  vid.hide(); 
 }
 
 function draw() {
   background(0);
   
-  
   if (c == 1){
   image(vid, 0, 0, width, height); 
+  audio.play();
 }
-if (c == 2){
-  image(vid2, 0, 0, width, height); 
+if (frameCount > 230){
+ image(vid2, 0, 0, width, height); 
+ audio2.play();
+ audio.speed(0.5);
+ audio1.play()
 }
-if (c == 3){
+if (frameCount > 370){
   image(vid3, 0, 0, width, height); 
+  audio3.play();
+  audio21.play();
+  audio2.speed(0.5);
+  audio.speed(0.25);
 }
-if (c == 4){
+if (frameCount > 500){
   image(vid4, 0, 0, width, height); 
+  audio4.play();
+  audio31.play();
+  audio3.speed(0.5);
+  audio2.speed(0.25);
+  audio.speed(0.1);
 }
-
 }
 
 function mousePressed() {
   c += 1; 
   if (c == 1){
-  vid.play(); 
-  }
-  if (frameCount > 100){ 
+  vid.play();
+  // c+=1;
+
+  if (frameCount > 230){ 
     vid2.play(); 
+    // c+=1;
   }
-  if (c == 3){ 
+  if (frameCount > 370){ 
     vid3.play(); 
+    // c+=1;
   }
-  if (c == 4){ 
+  if (frameCount > 500){ 
     vid4.play(); 
+  }
   }
   // if (c == 5){ 
   //   c === 0; 
   // }
-}
+} 
